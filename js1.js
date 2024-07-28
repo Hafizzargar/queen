2dafeac45f93864b08c2ee7ecdca9a5a7bbcc28d
Hafizzargar <hafezzargar987@gmail.com>
2024-07-28 11:20:53 +0530

    Describe your changes here

--- js1.js
+++ js1.js
@@ -3,8 +3,20 @@
 
 let loginContainer=document.querySelector('.login-container');
 let toggleButton=document.getElementById("toggleButton");
+let sho=document.getElementById("sho");
+let s1=document.getElementById("s1");
 let left=document.getElementById("left");
 let initbu=document.querySelector("#init>button");
+s1.addEventListener('click',()=>{
+    if(sho.style.display==='none'){
+        sho.style.display='block';
+
+    }else{
+        sho.style.display='none';
+
+    }
+    
+})
 function le(e){
     e.style.display="none";
     left.style.display='block';
